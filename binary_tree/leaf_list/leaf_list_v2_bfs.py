276a0dea07d7aa309a3fcd2e024ca8bd0e501ddb
Anirudh Suresh Ramchandran <anirudh.ramchandran@sifive.com>
2023-12-17 16:13:30 -0500

    added new method using bfs

---
+++ binary_tree/leaf_list/leaf_list_v2_bfs.py
@@ -0,0 +1,24 @@
+from collections import deque 
+def leaf_list(root):
+    if root is None:
+        return []
+    
+    q=deque()
+    q.append([root])
+
+    leaf_list=[]  # to store results 
+    
+    while q:
+        
+        current = q.popleft()
+
+        if not current.left and not current.right:
+            leaf_list.append(current.val)
+        
+        if current.left is not None:
+            q.append(current.left)  
+
+        if current.right is not None:
+            q.append(current.right) 
+    
+    return leaf_list
